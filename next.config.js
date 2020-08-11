52636ef2bdc4527b7d324128dbc1ae6c8b404896
bryan-atencia <bryan.atencia@thetiebar.com>
2020-08-11 09:24:18 -0500

    init

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack')
+const withReactStorefront = require('react-storefront/plugins/withReactStorefront')
+require('dotenv').config()
+
+// const withXDN = require('@xdn/next/withXDN')
+
+  // module.exports = withXDN({
+  //   // additional Next.js config options here
+  // })
+
+module.exports = withReactStorefront({
+  target: 'serverless',
+  connector: 'react-storefront/mock-connector',
+  webpack: config => {
+    config.plugins.push(
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      })
+    )
+    return config
+  },
+})
